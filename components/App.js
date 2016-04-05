593b7bbde2a8332927bc351135f69fd3f143a47d
helgaloa <helgaloa@gmail.com>
2016-04-05 10:01:29 +0000

    part 4

--- components/App.js
+++ components/App.js
@@ -3,9 +3,8 @@ import React, { Component } from 'react'
 class App extends Component {
 
   render() {
-    return <div>This is definitely a React app now!</div>
+    return <div>This is definitely a hotReact app now!</div>
   }
-
 }
 
 export default App
